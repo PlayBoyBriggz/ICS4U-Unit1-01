9a851aa9ce2566dcb0feb734705d3cd8094c0109
PlayBoybriggz <chris.briglio@gmail.com>
2020-02-11 17:59:12 +0000

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//Christian Briglio
+//feb 10 2020
+//The HelloWorld program implements an application that simply displays "Hello World!" to the standard output.
+public class HelloWorld {
+	public static void main(String[] args) {
+    	System.out.println("Hello World");
+	}
+}
